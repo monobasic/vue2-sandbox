101af75e91dfc20ee39f74ce4963db9cb5aeed97
André Abt <info@andre-abt.com>
2020-05-29 07:56:15 +0200

    Addd composition api

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import PortalVue from 'portal-vue'
+import VueCompositionApi from '@vue/composition-api';
 
 import './styles/index.css';
 
 Vue.use(PortalVue)
+Vue.use(VueCompositionApi);
 
 Vue.config.productionTip = false
 
