32c3fd30e95cfe323fad2caf78148043e7ebd119
André Abt <info@andre-abt.com>
2020-05-29 08:40:48 +0200

    Add ref example

---
+++ src/components/Card.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="max-w-sm mx-auto rounded overflow-hidden shadow-lg">
+  <img class="w-full" src="@/assets/sunset.jpg" alt="Sunset in the mountains">
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">{{ title }}</div>
+    <p class="text-gray-700 text-base">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+    </p>
+  </div>
+  <div class="px-6 py-4">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+  </div>
+</div>
+  </div>
+</template>
+<script>
+import { ref } from "@vue/composition-api";
+
+export default {
+  setup() {
+    const title = ref("Composed, reactive Title, yay!");
+
+    setTimeout(() => {
+      title.value = "Updated, composed, reactive Title, yay!";
+    }, 5000);
+
+    return { title };
+  }
+};
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,10 @@
 
     <h1>I am the Home component.</h1>
 
+    <Card></Card>
+
+    <br><br>
+
     <Notification>
       <template v-slot:title="{mySlotProp}">
         Here might be a page title. Here we have mySlotProp: {{ mySlotProp }}
@@ -10,15 +14,21 @@
       Hello I am the Notification Text passed in by slot
     </Notification>
 
+    <br><br>
+
     <List :items="items">
       <template v-slot:default="{item}">
         <div class="p3">icon {{ item.title }}</div>
       </template>
     </List>
 
+    <br><br>
+
     <List :items="items" v-slot:default="{item}">
         <div class="p3">icon {{ item.title }}</div>
     </List>
+
+    <br><br>
 
     <List :items="items">
       <template #default="{item}">
@@ -31,11 +41,13 @@
 <script>
 import Notification from './Notification.vue'
 import List from './List.vue'
+import Card from './Card.vue'
 
 export default {
   components: {
     Notification,
-    List
+    List,
+    Card,
   },
   data: () => {
     return {
