114b2feea8df2edc57f9ce286c19ae1a9ef9b747
André Abt <info@andre-abt.com>
2020-05-29 09:00:05 +0200

    Auto format changes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,41 +1,45 @@
 <template>
   <div>
     <div class="max-w-sm mx-auto rounded overflow-hidden shadow-lg">
-  <img class="w-full" src="@/assets/sunset.jpg" alt="Sunset in the mountains">
-  <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">{{ title }}</div>
-    <p class="text-gray-700 text-base">
-      Count: {{ count }}
-    </p>
-  </div>
-  <div class="px-6 py-4">
-    <button @click="increaseCount()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Increase count +
-    </button>
-  </div>
-</div>
+      <img
+        class="w-full"
+        src="@/assets/sunset.jpg"
+        alt="Sunset in the mountains"
+      />
+      <div class="px-6 py-4">
+        <div class="font-bold text-xl mb-2">{{ title }}</div>
+        <p class="text-gray-700 text-base">Count: {{ count }}</p>
+      </div>
+      <div class="px-6 py-4">
+        <button
+          @click="increaseCount()"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Increase count +
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import { ref } from "@vue/composition-api";
+import { ref } from '@vue/composition-api'
 
 export default {
   setup() {
     // Reactive reference example
-    const title = ref("Composed, reactive Title, yay!");
+    const title = ref('Composed, reactive Title, yay!')
     setTimeout(() => {
-      title.value = "Updated, composed, reactive Title, yay!";
-    }, 5000);
+      title.value = 'Updated, composed, reactive Title, yay!'
+    }, 5000)
 
-    const count = ref(0);
+    const count = ref(0)
 
     // Method example
     function increaseCount() {
-      count.value++;
+      count.value++
     }
 
-
-    return { title, count, increaseCount };
+    return { title, count, increaseCount }
   }
-};
+}
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,41 +1,39 @@
 <template>
   <div class="p-6">
-
     <h1>I am the Home component.</h1>
 
     <Card></Card>
 
-    <br><br>
+    <br /><br />
 
     <Notification>
-      <template v-slot:title="{mySlotProp}">
+      <template v-slot:title="{ mySlotProp }">
         Here might be a page title. Here we have mySlotProp: {{ mySlotProp }}
       </template>
       Hello I am the Notification Text passed in by slot
     </Notification>
 
-    <br><br>
+    <br /><br />
 
     <List :items="items">
-      <template v-slot:default="{item}">
+      <template v-slot:default="{ item }">
         <div class="p3">icon {{ item.title }}</div>
       </template>
     </List>
 
-    <br><br>
+    <br /><br />
 
-    <List :items="items" v-slot:default="{item}">
-        <div class="p3">icon {{ item.title }}</div>
+    <List :items="items" v-slot:default="{ item }">
+      <div class="p3">icon {{ item.title }}</div>
     </List>
 
-    <br><br>
+    <br /><br />
 
     <List :items="items">
       <template #default="{item}">
         <div class="p3">icon {{ item.title }}</div>
       </template>
     </List>
-
   </div>
 </template>
 <script>
@@ -47,7 +45,7 @@ export default {
   components: {
     Notification,
     List,
-    Card,
+    Card
   },
   data: () => {
     return {
@@ -60,7 +58,7 @@ export default {
         },
         {
           title: 'baz'
-        },
+        }
       ]
     }
   }
