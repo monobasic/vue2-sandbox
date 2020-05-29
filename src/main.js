d049347ec51ed8bf297847062c5af7511f5f5ba3
André Abt <info@andre-abt.com>
2020-05-29 11:59:51 +0200

    Code layout

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import PortalVue from 'portal-vue'
-import VueCompositionApi from '@vue/composition-api';
+import VueCompositionApi from '@vue/composition-api'
 
-import './styles/index.css';
+import './styles/index.css'
 
 Vue.use(PortalVue)
-Vue.use(VueCompositionApi);
+Vue.use(VueCompositionApi)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
