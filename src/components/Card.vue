3fe8f82dabb56604cba5ee3f8111262a0f1ba4ac
André Abt <info@andre-abt.com>
2020-05-29 08:50:31 +0200

    Add method example

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,13 +5,13 @@
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">{{ title }}</div>
     <p class="text-gray-700 text-base">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      Count: {{ count }}
     </p>
   </div>
   <div class="px-6 py-4">
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+    <button @click="increaseCount()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Increase count +
+    </button>
   </div>
 </div>
   </div>
@@ -21,13 +21,21 @@ import { ref } from "@vue/composition-api";
 
 export default {
   setup() {
+    // Reactive reference example
     const title = ref("Composed, reactive Title, yay!");
-
     setTimeout(() => {
       title.value = "Updated, composed, reactive Title, yay!";
     }, 5000);
 
-    return { title };
+    const count = ref(0);
+
+    // Method example
+    function increaseCount() {
+      count.value++;
+    }
+
+
+    return { title, count, increaseCount };
   }
 };
 </script>
